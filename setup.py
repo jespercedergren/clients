3e4b23816d9d7af5e0b8ee2794ae5cb802a86a4a
Jesper Cedergren <jespercedergren@Jespers-MacBook-Pro.local>
2020-04-20 09:13:36 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='clients',
+   version='0.1',
+   author='Jesper Cedergren',
+   author_email='jesper.cedergren@hotmail.com',
+   packages=['clients']  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
