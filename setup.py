1b3b905d53d0f4246f0e40484a6978b2a4b1990a
Jesper Cedergren <jespercedergren@Jespers-MacBook-Pro.local>
2020-04-20 10:00:32 +0200

    Added dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
    version='0.1',
    author='Jesper Cedergren',
    author_email='jesper.cedergren@hotmail.com',
-   packages=['clients']  #same as name
-   #install_requires=['bar', 'greek'], #external packages as dependencies
+   packages=['clients'],  #same as name
+   install_requires=['s3fs', 'fastparquet', 'toolz', 'dask', 'pyarrow'] #external packages as dependencies
 )
